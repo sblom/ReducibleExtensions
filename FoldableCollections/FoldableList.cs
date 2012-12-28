79de8c09461aa2c076d679d35aa721472e51141c
Scott Blomquist <scott@blomqui.st>
2012-12-27 23:21:47 -0800

    Initial commit.
    We've got:
    * NuGet set to auto-restore packages,
    * Some test cases based on NUnit,
    * A BSD-style license, and
    * A really boring AsFoldable() extension method.

---
+++ FoldableCollections/FoldableList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldableCollections
+{
+  internal class FoldableList<T>: IFoldable<T>
+  {
+    public TResult Fold<TResult>(Func<TResult, T, TResult> reducer)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ FoldableCollections/IFoldable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldableCollections
+{
+  public interface IFoldable<T>
+  {
+    TResult Fold<TResult>(Func<TResult, T, TResult> reducer);
+  }
+}
